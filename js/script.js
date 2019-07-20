15a073f4dcdfaf47a35c07a8b40248bf93d004b7
Bey Faith <fryumugabe@gmail.com>
2019-07-20 12:00:16 +0200

    add male names.

--- js/script.js
+++ js/script.js
@@ -8,18 +8,35 @@ function akan(){
   var day = parseFloat(document.getElementById("day").value);
   console.log(day);
   
-  var century = year/100+1;
+  var century = (year/100);
   console.log(century);
   
-  var birthday = Math.round( ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7);
+  var birthday = Math.round( ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7) ;
   console.log(birthday);
 
   var gender = document.getElementById("gender").value;
   console.log(gender);
 
-  var male= ["Kwasi", "Kwando", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var male= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   
   var female = ["Akosua","Adwoa","Abenaa","Akua","Yaa", "Afua","Ama"];
+
+  if (birthday ===1 && gender==="male"){
+    document.getElementById("display").innerHTML = male [1];
+
+  }
+  if(birthday===0 && gender ==="male") {
+    document.getElementById("display").innerHTML = male [0]; 
+  }
+  if(birthday===2 && gender ==="male") {
+    document.getElementById("display").innerHTML = male [2];
+  }
+  if(birthday===3 && gender==="male") {
+    document.getElementById("display").innerHTML = male [3];
+  }
+  if(birthday===4 && gender==="male") {
+    document.getElementById("display").innerText = male [4];
+  }
 
   
 }
